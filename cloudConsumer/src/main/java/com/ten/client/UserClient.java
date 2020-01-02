01891a862a6040e373dd2c10f2cc774eef341295
Peter Ren <408099844@qq.com>
2020-01-02 15:56:29 +0800

    learn feign fallback

--- cloudConsumer/src/main/java/com/ten/client/UserClient.java
+++ cloudConsumer/src/main/java/com/ten/client/UserClient.java
@@ -1,12 +1,14 @@
 package com.ten.client;
 
+import com.ten.conf.FeignLogConfiguration;
 import com.ten.entity.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "ten-article") // 标注该类是一个feign接口
+@FeignClient(value = "ten-article", fallback = UserClientFallback.class, configuration = FeignLogConfiguration.class)
+// 标注该类是一个feign接口
 public interface UserClient {
     // 两个坑：1. @GetMapping不支持   2. @PathVariable得设置value
     @RequestMapping(value = "/article/{id}", method = RequestMethod.GET)
---
+++ cloudConsumer/src/main/java/com/ten/conf/FeignLogConfiguration.java
@@ -0,0 +1,14 @@
+package com.ten.conf;
+
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignLogConfiguration {
+
+    @Bean
+    Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+}
